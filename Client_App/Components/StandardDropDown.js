f8b6eec86c59967099284504aef43e35d8495e7c
ryanwilliamsnj1 <49706876+ryanwilliamsnj1@users.noreply.github.com>
2019-11-01 15:27:08 -0400

    adding my componenets
    files

--- Client_App/Components/StandardDropDown.js
+++ Client_App/Components/StandardDropDown.js
@@ -1,18 +1,41 @@
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Picker, StyleSheet } from 'react-native';
 
-export default class StandardDropDown extends Component {
+const styles = StyleSheet.create({
+    texts: {
+      fontSize: 20,
+    },
+    inputs: {
+        width: '80%',
+        borderWidth: 2,
+        fontSize: 15,
+    },
+  });
+
+export default class StandardDropdown extends Component {
   constructor(props) {
     super(props);
+    this.options = this.props.options;
     this.state = {};
   }
 
+  optionList = () =>{
+    return( this.options.map( (x,i) => { 
+          return( <Picker.Item label={x} key={i} value={x}  />)} ));
+  }
+
   render() {
-    const {selectedValue, value, parentText, name} = this.props;
+    const {parentText, name, value, onValueChange, selectedValue} = this.props;
     return (
       <View>
-        <Text>{this.props.parentText}</Text>
-        <Picker></Picker>
+        <Text style={styles.texts}>{this.props.parentText}</Text>
+        <Picker
+            style={styles.inputs}
+            selectedValue = {this.props.value}
+            mode="dropdown"
+            onValueChange={value => onValueChange(name, value)}>
+            {this.optionList()}
+        </Picker>
       </View>
     );
   }
